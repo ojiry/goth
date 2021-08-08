7e894f0106f6d4cad94ae9a697ac9d9062f541b3
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2021-08-09 08:14:52 +0900

    Implement token endpoint skeleton

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -57,11 +58,40 @@ func (authenticateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/ping", http.StatusFound)
 }
 
+type tokenHandler struct{}
+
+func (tokenHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "no-store")
+	w.Header().Set("Pragma", "no-cache")
+
+	w.WriteHeader(http.StatusOK)
+
+	resp := struct {
+		AccessToken  string `json:"access_token"`
+		TokenType    string `json:"token_type"`
+		RefreshToken string `json:"refresh_token"`
+		ExpiresIn    int64  `json:"expires_in"`
+		IDToken      string `json:"id_token"`
+	}{
+		AccessToken:  "SlAV32hkKG",
+		TokenType:    "Bearer",
+		RefreshToken: "8xLOxBtZp8",
+		ExpiresIn:    3600,
+		IDToken:      "eyJhbGciOiJSUzI1NiIsImtpZCI6IjFlOWdkazcifQ.ewogImlzcyI6ICJodHRwOi8vc2VydmVyLmV4YW1wbGUuY29tIiwKICJzdWIiOiAiMjQ4Mjg5NzYxMDAxIiwKICJhdWQiOiAiczZCaGRSa3F0MyIsCiAibm9uY2UiOiAibi0wUzZfV3pBMk1qIiwKICJleHAiOiAxMzExMjgxOTcwLAogImlhdCI6IDEzMTEyODA5NzAKfQ.ggW8hZ1EuVLuxNuuIJKX_V8a_OMXzR0EHR9R6jgdqrOOF4daGU96Sr_P6qJp6IcmD3HP99Obi1PRs-cwh3LO-p146waJ8IhehcwL7F09JdijmBqkvPeB2T9CJNqeGpe-gccMg4vfKjkM8FcGvnzZUN4_KSP0aAp1tOJ1zZwgjxqGByKHiOtX7TpdQyHE5lcMiKPXfEIQILVq0pc_E2DzL7emopWoaoZTF_m0_N0YzFC6g6EJbOEoRoSK5hoDalrcvRYLSrQAZZKflyuVCyixEoV9GfNQC3_osjzw2PAithfubEEBLuVVk4XUVrWOLrLl0nx7RkKU8NXNHq-rvKMzqg",
+	}
+
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
 func NewServer() *http.Server {
 	mux := http.NewServeMux()
 	mux.Handle("/ping", pingHandler{})
 	mux.Handle("/authorize", authorizeHandler{})
 	mux.Handle("/authenticate", authenticateHandler{})
+	mux.Handle("/token", tokenHandler{})
 
 	s := &http.Server{
 		Addr:           ":8080",
